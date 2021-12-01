b02a9963eeafbf154accb16496c080a9a8ad8f5d
amir <amirzare309@yahoo.com>
2021-12-01 01:17:32 -0500

    create Project form.

---
+++ projects/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from .models import Project
+
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ('title', 'description', 'demoLink',
+                  'sourceLink', 'tags', 'voteTotal', 'voteRatio')
