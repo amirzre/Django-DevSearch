40c6b005546231e8db51ddc5b0f34e2034ef342f
amir <amirzare309@yahoo.com>
2021-12-04 04:01:08 -0500

    create login functionality.

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,6 @@ app_name = 'users'
 urlpatterns = [
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>/', views.userProfile, name='user_profile'),
+    path('/login', views.loginUser, name='login'),
+    path('/logout', views.logout, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Profile
 
 
@@ -15,3 +19,34 @@ def userProfile(request, pk):
     context = {'profile': profile,
                'topSkills': topSkills, 'otherSkills': otherSkills}
     return render(request, 'users/user_profile.html', context)
+
+
+def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('users:profiles')
+    if request.method == "POST":
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist!')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(
+                request.GET['next'] if 'next' in request.GET else 'account')
+
+        else:
+            messages.error(request, 'Username OR password is incorrect!')
+
+    return render(request, 'users/login_register.html')
+
+
+def logoutUser(request):
+    logout(request)
+    messages.info(request, 'User was logged out!')
+    return render(request, 'users/logout.html')
