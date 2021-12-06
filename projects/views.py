60e8e8eb64a2337dc13538d57d4be037f6351c6f
amir <amirzare309@yahoo.com>
2021-12-06 10:24:26 -0500

    creat search functionality.

--- projects/views.py
+++ projects/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Project
 from .forms import ProjectForm
+from .utils import searchProjects
 
 
 def projects(request):
-    projects = Project.objects.all()
+    projects, search_query = searchProjects(request)
     context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
--- users/views.py
+++ users/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .models import Profile
+from django.db.models import Q
+from .models import Profile, Skill
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .utils import searchProfiles
 
 
 def registerUser(request):
@@ -65,8 +67,8 @@ def logoutUser(request):
 
 
 def profiles(request):
-    profiles = Profile.objects.all()
-    context = {'profiles': profiles}
+    profiles, search_query = searchProfiles(request)
+    context = {'profiles': profiles, 'search_query': search_query}
     return render(request, 'users/profiles.html', context)
 
 
