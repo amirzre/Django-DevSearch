682397776dab07e2b4aa0fd8af0d403452ec4c52
amir <amirzare309@yahoo.com>
2021-12-16 10:38:45 -0500

    create forget password functionality.

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,29 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls', namespace='projects')),
     path('', include('users.urls', namespace='users')),
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(
+             template_name='reset_password.html'),
+         name='reset_password'),
+    path('reset_password_sent/',
+         auth_views.PasswordChangeDoneView.as_view(
+             template_name='reset_password_sent.html'),
+         name='passowrd_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='reset.html'),
+         name='passowrd_reset_confirm'),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='passowrd_reset_complete.html'),
+         name='passowrd_reset_complete'),
 ]
 
 if settings.DEBUG:
--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import Profile
 
 
@@ -11,6 +13,10 @@ def createProfile(sender, instance, created, **kwargs):
         profile = Profile.objects.create(
             user=user, username=user.username, email=user.email,
             name=user.first_name)
+        subject = 'welcome to DevSearch.'
+        message = 'we are glad you are here!'
+        send_mail(subject, message, settings.EMAIL_HOST_USER,
+                  [profile.email], fail_silently=False)
 
 
 @receiver(post_save, sender=Profile)
